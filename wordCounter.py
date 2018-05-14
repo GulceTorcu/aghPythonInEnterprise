d39881ba84fcf4aee3d03dadbf6065f56e6f9a06
GulceTorcu <gulcetorcu@hotmail.com>
2018-05-15 00:15:44 +0300

    Update

--- wordCounter.py
+++ wordCounter.py
@@ -1,18 +1,33 @@
-import os
+import argparse
 
-bcount=0
-filename = input('Enter a file name: ')
-if(not os.path.isfile(filename)):
-	print("File doesn't exists")
-else:
-	f=open(filename,'r')
-	content=f.read()
-	lcount=len(content.split("\n"))
-	print(lcount)
-	wcount=len(content.split(" "))
-	print(wcount)
-	for words in content.split(" "):
-		bcount=bcount+len(words)
-	print(bcount)
-	f.close()
+parser=argparse.ArgumentParser(description='Counts lines, words, letters')
+parser.add_argument('file', help="Name of the file that counted")
+parser.add_argument('-l', help="To count lines", action='store_true')
+parser.add_argument('-w', help="To count lines", action='store_true')
+parser.add_argument('-b', help="To count lines", action='store_true')
+args=parser.parse_args()
 
+try:
+    f=open(args.file)
+    lcount=0
+    wcount=0
+    bcount=0
+
+    for line in f:
+        for letter in line:
+            bcount=bcount+1
+        words=line.split()
+        wcount=wcount+len(words)
+        lcount=lcount+1
+    f.close()
+
+    if args.l:
+        print("Lines: " + str(lcount) + "\n")
+    if args.w:
+        print("Words: " + str(wcount) + "\n")
+    if args.b:
+        print("Characters: " + str(bcount) + "\n")
+
+
+except IOError:
+    print("The file does not exits.")
